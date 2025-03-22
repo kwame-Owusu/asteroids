170cfa74d2cac20b08b4188b97a26afb8962633e
kwame-Owusu <alfredowusu595@gmail.com>
2025-03-22 23:23:53 +0000

    added pygame screen creation and game loop

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-import pygame
 from constants import *
-
+import pygame
 
 def main():
+  pygame.init()
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  
+  while True:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        return
+    
+    black_color = (0,0,0)
+    screen.fill(color=black_color)
+    pygame.display.flip()
   print("Starting Asteroids!")
   print(f"Screen width: {SCREEN_WIDTH}")
   print(f"Screen height: {SCREEN_HEIGHT}")
