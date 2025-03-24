6d17174f885ad8d19e8c001e4d437aa1ab9f79a1
kwame-Owusu <alfredowusu595@gmail.com>
2025-03-24 23:00:17 +0000

    added cool down on the amount of bullets that can be shot using a timer mechanism

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 from Shot import Shot
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOT_SPEED, PLAYER_SHOOT_COOLDOWN
 import pygame
 
 class Player(CircleShape):
@@ -8,6 +8,8 @@ class Player(CircleShape):
   def __init__(self, x, y):
     super().__init__(x, y, PLAYER_RADIUS)
     self.rotation = 0
+    self.timer = 0
+    
   
   def triangle(self) -> list[int]:
     """
@@ -35,6 +37,7 @@ class Player(CircleShape):
     holding the key pressing logic using WASD
     """
     keys = pygame.key.get_pressed()
+    self.timer -= delta_time
 
     if keys[pygame.K_a]:
        self.rotate(-delta_time) 
@@ -47,6 +50,8 @@ class Player(CircleShape):
       self.move(-delta_time) 
       
     if keys[pygame.K_SPACE]:
+      if self.timer > 0:
+        return
       self.shoot()
   
   def move(self, delta_time: int) -> None:
@@ -57,5 +62,6 @@ class Player(CircleShape):
     self.position += forward * PLAYER_SPEED * delta_time
     
   def shoot(self) -> None:
+    self.timer = PLAYER_SHOOT_COOLDOWN
     new_shot = Shot(self.position.x, self.position.y)
     new_shot.velocity = pygame.math.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED 
